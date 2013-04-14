58063f3c544eaa4d8dc72893d01c52218fb6c2ef
Chris Duggins <inplayrs@gmail.com>
2013-04-14 12:01:58 +0100

    Changed GamePointsResponse ints to Integers to allow for null values

--- src/com/inplayrs/rest/responseds/GamePointsResponse.java
+++ src/com/inplayrs/rest/responseds/GamePointsResponse.java
@@ -12,20 +12,20 @@ public class GamePointsResponse {
 	// From game table
 	@Column(name = "global_pot_size")
 	@JsonProperty
-	private int global_pot_size;
+	private Integer global_pot_size;
 	
 	@Column(name = "fangroup_pot_size")
 	@JsonProperty
-	private int fangroup_pot_size;
+	private Integer fangroup_pot_size;
 	
 	// from global_game_leaderboard table
 	@Column(name = "points")
 	@JsonProperty
-	private int points;
+	private Integer points;
 	
 	@Column(name = "global_rank")
 	@JsonProperty
-	private int global_rank;
+	private Integer global_rank;
 	
 	// from fangroup_game_leaderboard table
 	@Column(name = "fangroup_name")
@@ -34,7 +34,7 @@ public class GamePointsResponse {
 	
 	@Column(name = "fangroup_rank")
 	@JsonProperty
-	private int fangroup_rank;
+	private Integer fangroup_rank;
 	
 	// from h2h_pool
 	@Column(name = "h2h_user")
@@ -43,91 +43,60 @@ public class GamePointsResponse {
 	
 	@Column(name = "h2h_pot_size")
 	@JsonProperty
-	private int h2h_pot_size;
+	private Integer h2h_pot_size;
 	
 	// from user_in_fangroup_game_leaderboard
 	@Column(name = "user_in_fangroup_rank")
 	@JsonProperty
-	private int user_in_fangroup_rank;
+	private Integer user_in_fangroup_rank;
 	
 	// from game_entry
 	@Column(name = "total_winnings")
 	@JsonProperty
-	private int total_winnings;
+	private Integer total_winnings;
 	
 	
 	public GamePointsResponse() {
-		/*
-		 * 
 
-select 
-	ge.user,
-	ge.game as game_id,
-	g.global_pot_size,
-	g.fangroup_pot_size,
-	h2h.h2h_pot_size,
-	ge.total_winnings,
-	(CASE 
-    WHEN h2h.game_entry_1 = ge.game_entry_id then h2h.user_2
-    WHEN h2h.game_entry_2 = ge.game_entry_id then h2h.user_1
-    ELSE null
-	END) as h2h_user,
-	ggl.fangroup_name as fangroup_name,
-	ggl.points,
-	ggl.rank as global_rank,
-	fgl.rank as fangroup_rank,
-	uifgl.rank as user_in_fangroup_rank
-from 
-	game_entry ge
-	left join game g on g.game_id = ge.game
-	left join h2h_pool h2h on ( (h2h.game_entry_1 = ge.game_entry_id) or
-										 (h2h.game_entry_2 = ge.game_entry_id) )
-	left join global_game_leaderboard ggl on (ggl.game = ge.game and ggl.user = ge.user)
-	left join fangroup_game_leaderboard fgl on (fgl.game = ge.game and fgl.fangroup_id = ggl.fangroup_id)
-	left join user_in_fangroup_game_leaderboard uifgl on (uifgl.game = ge.game and uifgl.user = ge.user)
-	
-										 
-										 
-		 */
 	}
 
 
-	public int getGlobal_pot_size() {
+	public Integer getGlobal_pot_size() {
 		return global_pot_size;
 	}
 
 
-	public void setGlobal_pot_size(int global_pot_size) {
+	public void setGlobal_pot_size(Integer global_pot_size) {
 		this.global_pot_size = global_pot_size;
 	}
 
 
-	public int getFangroup_pot_size() {
+	public Integer getFangroup_pot_size() {
 		return fangroup_pot_size;
 	}
 
 
-	public void setFangroup_pot_size(int fangroup_pot_size) {
+	public void setFangroup_pot_size(Integer fangroup_pot_size) {
 		this.fangroup_pot_size = fangroup_pot_size;
 	}
 
 
-	public int getPoints() {
+	public Integer getPoints() {
 		return points;
 	}
 
 
-	public void setPoints(int points) {
+	public void setPoints(Integer points) {
 		this.points = points;
 	}
 
 
-	public int getGlobal_rank() {
+	public Integer getGlobal_rank() {
 		return global_rank;
 	}
 
 
-	public void setGlobal_rank(int global_rank) {
+	public void setGlobal_rank(Integer global_rank) {
 		this.global_rank = global_rank;
 	}
 
@@ -142,12 +111,12 @@ from
 	}
 
 
-	public int getFangroup_rank() {
+	public Integer getFangroup_rank() {
 		return fangroup_rank;
 	}
 
 
-	public void setFangroup_rank(int fangroup_rank) {
+	public void setFangroup_rank(Integer fangroup_rank) {
 		this.fangroup_rank = fangroup_rank;
 	}
 
@@ -162,33 +131,33 @@ from
 	}
 
 
-	public int getH2h_pot_size() {
+	public Integer getH2h_pot_size() {
 		return h2h_pot_size;
 	}
 
 
-	public void setH2h_pot_size(int h2h_pot_size) {
+	public void setH2h_pot_size(Integer h2h_pot_size) {
 		this.h2h_pot_size = h2h_pot_size;
 	}
 
 
 
-	public int getTotal_winnings() {
+	public Integer getTotal_winnings() {
 		return total_winnings;
 	}
 
 
-	public void setTotal_winnings(int total_winnings) {
+	public void setTotal_winnings(Integer total_winnings) {
 		this.total_winnings = total_winnings;
 	}
 
 
-	public int getUser_in_fangroup_rank() {
+	public Integer getUser_in_fangroup_rank() {
 		return user_in_fangroup_rank;
 	}
 
 
-	public void setUser_in_fangroup_rank(int user_in_fangroup_rank) {
+	public void setUser_in_fangroup_rank(Integer user_in_fangroup_rank) {
 		this.user_in_fangroup_rank = user_in_fangroup_rank;
 	}
 
