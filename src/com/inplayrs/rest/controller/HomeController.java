25b34169175e4ae1b46e5a94982b8af8af6c75c3
Chris Duggins <inplayrs@gmail.com>
2013-07-03 23:05:36 +0100

    Upgraded mysql connector/j jar from 5.1.23 to 5.1.25

--- src/com/inplayrs/rest/controller/HomeController.java
+++ src/com/inplayrs/rest/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
  
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     
     public String home() {
         System.out.println("HomeController: Passing through...");
