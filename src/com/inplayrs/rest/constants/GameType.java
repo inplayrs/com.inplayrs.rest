78b1d1da0e1b56d91ac76981f3b6016d8ba30c30
Chris <chris@duggins.eu>
2014-07-27 18:56:35 +0100

    Changed values for quiz and fantasy game types

--- src/com/inplayrs/rest/constants/GameType.java
+++ src/com/inplayrs/rest/constants/GameType.java
@@ -1,9 +1,20 @@
 package com.inplayrs.rest.constants;
 
 public class GameType {
+	
+	public final static int WINAB = 20;
+	public final static int UPDOWN = 21;
+	public final static int THRUCUT = 22;
+	public final static int YESNO = 23;
+	public final static int OVERUNDER = 24;
+	public final static int HOMEAWAY = 25;
+	public final static int HOMEDRAWAWAY = 30;
+	public final static int TOP10 = 31;
+	public final static int TOP5 = 32;
+	public final static int TOP3 = 33;
+	public final static int LEADTHRUCUT = 34;
+	public final static int ZEROONETWO = 35;
+	public final static int QUIZ = 40;
+	public final static int FANTASY = 100;
 
-	public final static int YN = 20;  // Yes/No
-	public final static int HDA = 30; // Home/Draw/Away
-	public final static int QUIZ = 4;
-	public final static int FANTASY = 5;
 }
